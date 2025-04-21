67787655cbc867412ec64d03646571e9c4cbb94a
dhyanesh666 <63642559+dhyanesh666@users.noreply.github.com>
2025-04-21 02:30:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from ECS!", 200
+    return "Hello from terraform!", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
